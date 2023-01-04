af189f38cced368ef2c4f806a42bfb1bc903f843
babenk0ff <babenk0fff@gmail.com>
2023-01-04 17:14:35 +0300

    lesson-1

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from uuid import uuid4
+
+from django.db import models
+
+
+class User(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    username = models.CharField(max_length=64)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.CharField(max_length=64, unique=True)
