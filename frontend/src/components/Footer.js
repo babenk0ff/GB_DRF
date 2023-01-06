ab14771b06e6bae9d23da080f87b1a59a5b21754
babenk0ff <babenk0fff@gmail.com>
2023-01-07 00:58:34 +0300

    lesson-2

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function FooterItem() {
+    return (
+        <div>
+            <hr/>
+            <h3>Footer</h3>
+        </div>
+    )
+}
+
+export default FooterItem
---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function MenuItem() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <hr/>
+        </div>
+    )
+}
+
+export default MenuItem
