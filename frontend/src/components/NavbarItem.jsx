3c7431c4b6b899f1de9d86159b8428338ff0813d
stas <babenk0fff@gmail.com>
2023-01-29 06:25:27 -0800

    lesson-7

--- frontend/src/components/NavbarItem.jsx
+++ frontend/src/components/NavbarItem.jsx
@@ -4,7 +4,8 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-function NavbarItem() {
+
+function NavbarItem({isAuth, logout, username}) {
     return (
         <div>
             <Navbar className="mb-2" bg="dark" variant="dark">
@@ -17,12 +18,31 @@ function NavbarItem() {
                             className="d-inline-block align-top"
                             alt=""
                         />
+                        Project ToDo
                     </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Link className="nav-link" to='/'>Projects</Link>
-                        <Link className="nav-link" to='/users'>Users</Link>
-                        <Link className="nav-link" to='/todos'>ToDo</Link>
-                    </Nav>
+                    {isAuth() && (
+                        <React.Fragment>
+                            <Nav className="me-auto">
+                                <Link className="nav-link" to='/projects'>Projects</Link>
+                                <Link className="nav-link" to='/todos'>ToDo</Link>
+                                <Link className="nav-link" to='/users'>Users</Link>
+                            </Nav>
+                            <Nav className="ml-auto">
+                                <Link className="nav-link" to='/'>
+                                    <span>{username}</span>
+                                </Link>
+                                <span className="navbar-divider">|</span>
+                                <Link className="nav-link" to='/login'>
+                                    <span onClick={logout}>Logout</span>
+                                </Link>
+                            </Nav>
+                        </React.Fragment>
+                    )}
+                    {!isAuth() && (
+                        <Nav className="ml-auto">
+                            <Link className="nav-link" to='/login'>Login</Link>
+                        </Nav>
+                    )}
                 </Container>
             </Navbar>
         </div>
